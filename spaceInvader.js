1a271f12840908aeadd3967974b4861c14b5fd42
Julien De Reuse <julien.de.reuse@student.ehb.be>
2022-10-06 19:49:39 +0200

    Assignment 1: Space Invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,36 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+drawLine();
+
+function drawLine() {
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.rect(0,0,300,300);
+    context.fill();
+
+    context.fillStyle = 'rgb(0,255,0)';
+    context.beginPath();
+    context.rect(25,25,250,50);
+    context.fill();
+
+    context.fillStyle = 'rgb(0,255,0)';
+    context.beginPath();
+    context.rect(25,175,50,100);
+    context.fill();
+
+    context.fillStyle = 'rgb(0,255,0)';
+    context.beginPath();
+    context.rect(125,175,50,50);
+    context.fill();
+
+    context.fillStyle = 'rgb(0,255,0)';
+    context.beginPath();
+    context.rect(225,175,50,100);
+    context.fill();
+
+}
